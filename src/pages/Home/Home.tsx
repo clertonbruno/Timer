37a062115f8c81eea636c8143e043b7a095a0b12
ClertonDruid <clerton.sampaio@druid.com.br>
2022-11-14 15:27:33 -0300

    add form validation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,53 @@
 import { Play } from 'phosphor-react';
 import * as S from './Home.styles';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
+
+const newTimerFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Task is required'),
+  minutesDuration: zod
+    .number()
+    .min(5, 'Timer cannot be less than 5 minutes')
+    .max(60, 'Timer cannot be more than 60 minutes'),
+});
+
+// to create a type NewTimerFormValues we can use zod's infer method
+type NewTimerFormValues = zod.infer<typeof newTimerFormValidationSchema>;
+
+// // or we can create a type (or interface) manually
+// interface NewTimerFormValues {
+//   task: string;
+//   minutesDuration: number;
+// }
 
 export const Home = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<NewTimerFormValues>({
+    resolver: zodResolver(newTimerFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesDuration: 0,
+    },
+  });
+
+  const onSubmit = (data: NewTimerFormValues) => {
+    console.log('Submitted info provided by the handleSubmit > ', data);
+    reset();
+  };
+
+  const task = watch('task');
+
+  const shouldEnableSubmit = task && task.length > 0;
+
   return (
     <S.HomeContainer>
-      <form action=''>
+      <form action='' onSubmit={handleSubmit(onSubmit)}>
         <S.FormHeader>
           <label htmlFor='task'>Will work on</label>
           <S.TaskInput
@@ -12,6 +55,7 @@ export const Home = () => {
             type='text'
             list='task-list'
             placeholder='Task name'
+            {...register('task', { required: true })}
           />
           <datalist id='task-list'>
             <option value='Task 1' />
@@ -28,6 +72,10 @@ export const Home = () => {
             step={5}
             min={5}
             max={60}
+            {...register('minutesDuration', {
+              required: true,
+              valueAsNumber: true,
+            })}
           />
 
           <span>minutes.</span>
@@ -40,7 +88,7 @@ export const Home = () => {
           <span>0</span>
         </S.CountdownContainer>
 
-        <S.CountdownButton type='submit' disabled>
+        <S.CountdownButton type='submit' disabled={!shouldEnableSubmit}>
           <Play size={24} />
           Start
         </S.CountdownButton>
